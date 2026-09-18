fba6c686363b5f8566cfe3b8d55c285b63714fc2
zerachiel016 <z.dadsi21@gmail.com>
2026-09-18 14:59:51 +0100

    fix: block connections instead of zones

--- pathfinder.py
+++ pathfinder.py
@@ -16,7 +16,7 @@ class Pathfinder:
         return path
 
     @staticmethod
-    def djikstra(hubs, blocked):
+    def djikstra(hubs, blocked_con):
         table: dict[str, tuple[float, str | None]] = {h.name: (0 if h.start else float("inf"), None) for h in hubs.values()}
         queue: list[tuple[float, str | None]] = [(0, next(h.name for h in hubs.values() if h.start))]
 
@@ -26,7 +26,7 @@ class Pathfinder:
                 return (Pathfinder.get_path_from_table(table, cur_zone), table[cur_zone][0])
             for con in [con[0] for con in hubs[cur_zone].connections]:
                 new_con_cost = cur_cost + hubs[con].cost
-                if new_con_cost < table[con][0] and hubs[con].zone != "blocked" and con != blocked:
+                if new_con_cost < table[con][0] and hubs[con].zone != "blocked" and {con, cur_zone} != blocked_con:
                     table[con] = (new_con_cost, cur_zone)
                     heapq.heappush(queue, (new_con_cost, con));
 
@@ -35,10 +35,9 @@ class Pathfinder:
     @staticmethod
     def get_second_path(main_path, hubs):
         paths: list[tuple[float, list[str]]] = []
-        for h in main_path:
-            if hubs[h].start or hubs[h].end:
-                continue
-            path = Pathfinder.djikstra(hubs, h)
+
+        for blocked_con in (set(t) for t in list(zip(main_path, main_path[1:]))):
+            path = Pathfinder.djikstra(hubs, blocked_con)
             if path[0]:
                 paths += [(path[1], path[0])]
 
